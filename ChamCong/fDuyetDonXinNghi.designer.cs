553c4fe9420a24863d6ed88c01865feb4d59a520
TrieuPhi <dtptrieuphidtp@gmail.com>
2023-04-30 17:26:25 +0700

    Trieuphi commit

--- ChamCong/fDuyetDonXinNghi.designer.cs
+++ ChamCong/fDuyetDonXinNghi.designer.cs
@@ -28,29 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pnldatagridviewdonxinnghi = new ArtanComponent.ArtanPannel();
             this.gvDSXinNghi = new System.Windows.Forms.DataGridView();
+            this.PnlDonxinnghi = new ArtanComponent.ArtanPannel();
             this.lblTitle = new System.Windows.Forms.Label();
+            this.pnldatagridviewdonxinnghi.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gvDSXinNghi)).BeginInit();
+            this.PnlDonxinnghi.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnldatagridviewdonxinnghi
+            // 
+            this.pnldatagridviewdonxinnghi.BackColor = System.Drawing.Color.White;
+            this.pnldatagridviewdonxinnghi.BorderRadius = 30;
+            this.pnldatagridviewdonxinnghi.Controls.Add(this.gvDSXinNghi);
+            this.pnldatagridviewdonxinnghi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(95)))), ((int)(((byte)(231)))));
+            this.pnldatagridviewdonxinnghi.GradientAngle = 90F;
+            this.pnldatagridviewdonxinnghi.GradientBttomColor = System.Drawing.Color.DarkOrchid;
+            this.pnldatagridviewdonxinnghi.GradientTopcolor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(95)))), ((int)(((byte)(231)))));
+            this.pnldatagridviewdonxinnghi.Location = new System.Drawing.Point(47, 158);
+            this.pnldatagridviewdonxinnghi.Name = "pnldatagridviewdonxinnghi";
+            this.pnldatagridviewdonxinnghi.Padding = new System.Windows.Forms.Padding(10);
+            this.pnldatagridviewdonxinnghi.Size = new System.Drawing.Size(820, 527);
+            this.pnldatagridviewdonxinnghi.TabIndex = 3;
             // 
             // gvDSXinNghi
             // 
-            this.gvDSXinNghi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvDSXinNghi.Location = new System.Drawing.Point(45, 179);
+            this.gvDSXinNghi.AllowUserToAddRows = false;
+            this.gvDSXinNghi.AllowUserToDeleteRows = false;
+            this.gvDSXinNghi.AllowUserToResizeColumns = false;
+            this.gvDSXinNghi.AllowUserToResizeRows = false;
+            this.gvDSXinNghi.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gvDSXinNghi.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.gvDSXinNghi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gvDSXinNghi.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(96)))), ((int)(((byte)(232)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(252)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(115)))), ((int)(((byte)(93)))), ((int)(((byte)(229)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(231)))), ((int)(((byte)(252)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gvDSXinNghi.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.gvDSXinNghi.ColumnHeadersHeight = 35;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(95)))), ((int)(((byte)(231)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(229)))), ((int)(((byte)(253)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(101)))), ((int)(((byte)(180)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gvDSXinNghi.DefaultCellStyle = dataGridViewCellStyle2;
+            this.gvDSXinNghi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gvDSXinNghi.EnableHeadersVisualStyles = false;
+            this.gvDSXinNghi.Location = new System.Drawing.Point(10, 10);
+            this.gvDSXinNghi.MultiSelect = false;
             this.gvDSXinNghi.Name = "gvDSXinNghi";
+            this.gvDSXinNghi.RowHeadersVisible = false;
             this.gvDSXinNghi.RowHeadersWidth = 51;
             this.gvDSXinNghi.RowTemplate.Height = 24;
-            this.gvDSXinNghi.Size = new System.Drawing.Size(424, 518);
+            this.gvDSXinNghi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gvDSXinNghi.Size = new System.Drawing.Size(800, 507);
             this.gvDSXinNghi.TabIndex = 0;
             this.gvDSXinNghi.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.gvDSXinNghi_RowHeaderMouseClick);
             // 
+            // PnlDonxinnghi
+            // 
+            this.PnlDonxinnghi.BackColor = System.Drawing.Color.White;
+            this.PnlDonxinnghi.BorderRadius = 50;
+            this.PnlDonxinnghi.Controls.Add(this.lblTitle);
+            this.PnlDonxinnghi.ForeColor = System.Drawing.Color.Black;
+            this.PnlDonxinnghi.GradientAngle = 90F;
+            this.PnlDonxinnghi.GradientBttomColor = System.Drawing.Color.DarkOrchid;
+            this.PnlDonxinnghi.GradientTopcolor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(95)))), ((int)(((byte)(231)))));
+            this.PnlDonxinnghi.Location = new System.Drawing.Point(322, 30);
+            this.PnlDonxinnghi.Name = "PnlDonxinnghi";
+            this.PnlDonxinnghi.Padding = new System.Windows.Forms.Padding(10);
+            this.PnlDonxinnghi.Size = new System.Drawing.Size(667, 78);
+            this.PnlDonxinnghi.TabIndex = 2;
+            // 
             // lblTitle
             // 
-            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Location = new System.Drawing.Point(10, 10);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(1355, 93);
+            this.lblTitle.Size = new System.Drawing.Size(647, 58);
             this.lblTitle.TabIndex = 1;
             this.lblTitle.Text = "DANH SÁCH ĐƠN XIN NGHỈ";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -61,13 +127,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(56)))), ((int)(((byte)(72)))));
             this.ClientSize = new System.Drawing.Size(1355, 819);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.gvDSXinNghi);
+            this.Controls.Add(this.pnldatagridviewdonxinnghi);
+            this.Controls.Add(this.PnlDonxinnghi);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "fDuyetDonXinNghi";
+            this.Padding = new System.Windows.Forms.Padding(10);
             this.Text = "Duyệt đơn xin nghỉ";
             this.Load += new System.EventHandler(this.fDuyetDonXinNghi_Load);
+            this.pnldatagridviewdonxinnghi.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gvDSXinNghi)).EndInit();
+            this.PnlDonxinnghi.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -76,5 +145,7 @@
 
         private System.Windows.Forms.DataGridView gvDSXinNghi;
         private System.Windows.Forms.Label lblTitle;
+        private ArtanComponent.ArtanPannel PnlDonxinnghi;
+        private ArtanComponent.ArtanPannel pnldatagridviewdonxinnghi;
     }
 }
